9baad4c90184097ed7b87dfad908a61956f599eb
Allex372 <alexalex372.aa@gmail.com>
2023-08-10 20:57:52 +0300

    fix

--- src/components/ProdactsLayout/ProductCard/ProductCard.js
+++ src/components/ProdactsLayout/ProductCard/ProductCard.js
@@ -6,6 +6,7 @@ import { useBacket } from "../../../context/BacketProvider";
 import * as styles from './ProductCard.module.css'
 
 export const ProductCard = ({ product, viewStyle }) => {
+    console.log(product);
     const backetContext = useBacket();
     const { addItemToBacket, items, handleOpenBacket } = backetContext ? backetContext : {};
 
