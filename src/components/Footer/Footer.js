fc0e9f863ea7494126ee02813616e603f01a0e75
oleksandr <alexalex372.aa@gmail.com>
2024-04-09 20:06:47 +0300

    add social refs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,60 @@
-import * as React from "react";
-import logo from '../../images/logo.png';
-import tg from '../../images/social/tg.svg';
-import vib from '../../images/social/vib.svg';
-import wa from '../../images/social/wa.svg';
+import * as React from "react"
+import logo from "../../images/logo.png"
+import tg from "../../images/social/tg.svg"
+import vib from "../../images/social/vib.svg"
+import wa from "../../images/social/wa.svg"
 
-import * as styles from './Footer.module.css';
+import * as styles from "./Footer.module.css"
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone } from "@fortawesome/free-solid-svg-icons"
 
 export const Footer = () => {
-    return (
-        <div className={styles.wrapper}>
-            <div>
-                <img src={logo} alt='logo' className={styles.logo} />
-            </div>
-            <div className={styles.socialWrapper}>
-                {/* <p>Ми у месенджері:</p>
-                <div className={styles.iconWrapper}>
-                    <img src={tg} alt='telegram icon' />
-                    <img src={vib} alt='viber icon' />
-                    <img src={wa} alt='whats up icon' />
-                </div> */}
-            </div>
-            <div className={styles.lastWrapper}>
-                <button className={styles.btnCall}>
-                    <a href="tel:+380674711434" target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faPhone} size="lg" className={styles.iconPhone} />
-                    </a>
-                    Зателефонувати
-                </button>
-                <p className={styles.email}>agrozakhyst.vovk@gmail.com</p>
-                {/* <p className={styles.dillers}>Виробники:
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <img src={logo} alt="logo" className={styles.logo} />
+      </div>
+      <div className={styles.socialWrapper}>
+        <p>Ми у месенджері:</p>
+        <div className={styles.iconWrapper}>
+          <a href="https://t.me/+380674711434" target="_blank" rel="noreferrer">
+            <img alt="social" src={tg} />
+          </a>
+
+          <a
+            href="viber://chat?number=%2B380674711434"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img alt="social" src={vib} />
+          </a>
+
+          <a href="https://wa.me/380674711434" target="_blank" rel="noreferrer">
+            <img alt="social" src={wa} />
+          </a>
+        </div>
+      </div>
+      <div className={styles.lastWrapper}>
+        <a
+          href="tel:+380674711434"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.btnCall}
+        >
+          <FontAwesomeIcon
+            icon={faPhone}
+            size="lg"
+            className={styles.iconPhone}
+          />
+          Зателефонувати
+        </a>
+
+        <p className={styles.email}>agrozakhyst.vovk@gmail.com</p>
+        {/* <p className={styles.dillers}>Виробники:
                     <a href="https://agrohimteh.com.ua/" className={styles.link} target='blank'> Агрохімічні технології</a>
                 </p> */}
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,76 +1,78 @@
-import React, { useState, useEffect } from "react";
-import { useBacket } from "../../context/BacketProvider";
-import { useResult } from "../../context/SearchResultProvider";
-import { useSideBar } from "../../context/SideBarProvider";
-import { useFilter } from "../../context/FilterProvider";
-import { navigate } from "gatsby";
-import { ModalSearch } from "../ModalSearch/ModalSearch";
+import React, { useState, useEffect } from "react"
+import { useBacket } from "../../context/BacketProvider"
+import { useResult } from "../../context/SearchResultProvider"
+import { useSideBar } from "../../context/SideBarProvider"
+import { useFilter } from "../../context/FilterProvider"
+import { navigate } from "gatsby"
+import { ModalSearch } from "../ModalSearch/ModalSearch"
 
-import * as styles from './header.module.css';
+import * as styles from "./header.module.css"
 
-import tg from '../../images/social/tg.svg';
-import vib from '../../images/social/vib.svg';
-import wa from '../../images/social/wa.svg';
-import backet from '../../images/purchase.png';
-import close from '../../images/close.png';
-import searchIcon from '../../images/search.svg';
-import newLogo from '../../images/logo.png';
+import tg from "../../images/social/tg.svg"
+import vib from "../../images/social/vib.svg"
+import wa from "../../images/social/wa.svg"
+import backet from "../../images/purchase.png"
+import close from "../../images/close.png"
+import searchIcon from "../../images/search.svg"
+import newLogo from "../../images/logo.png"
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone } from "@fortawesome/free-solid-svg-icons"
 
 export const Header = () => {
-  const resultFilterContext = useFilter();
-  const { changeCultureFilter, changeChemistryFilter, changeTypeFilter } = resultFilterContext ? resultFilterContext : {};
+  const resultFilterContext = useFilter()
+  const { changeCultureFilter, changeChemistryFilter, changeTypeFilter } =
+    resultFilterContext ? resultFilterContext : {}
 
-  const resultContext = useResult();
-  const { changeSearchResult, searchResult } = resultContext ? resultContext : {};
+  const resultContext = useResult()
+  const { changeSearchResult, searchResult } = resultContext
+    ? resultContext
+    : {}
 
-  const backetContext = useBacket();
-  const { handleOpenBacket, items } = backetContext ? backetContext : {};
+  const backetContext = useBacket()
+  const { handleOpenBacket, items } = backetContext ? backetContext : {}
 
-  const sideBarContext = useSideBar();
-  const { menuStatus, setMenuStatus } = sideBarContext ? sideBarContext : {};
+  const sideBarContext = useSideBar()
+  const { menuStatus, setMenuStatus } = sideBarContext ? sideBarContext : {}
 
-  const [currentPath, setCurrentPath] = useState('');
-  const [mobileSeacrhModal, setMobileSearchModal] = useState(false);
+  const [currentPath, setCurrentPath] = useState("")
+  const [mobileSeacrhModal, setMobileSearchModal] = useState(false)
 
-  const [style, setStyle] = useState(styles.menu);
-
+  const [style, setStyle] = useState(styles.menu)
 
   useEffect(() => {
-    setCurrentPath(window.location.pathname);
-  }, []);
+    setCurrentPath(window.location.pathname)
+  }, [])
 
   useEffect(() => {
     if (menuStatus) {
-      document.body.classList.add(styles.bodyNoScroll);
-      setStyle(`${styles.menu} ${styles.active}`);
+      document.body.classList.add(styles.bodyNoScroll)
+      setStyle(`${styles.menu} ${styles.active}`)
     } else {
-      document.body.classList.remove(styles.bodyNoScroll);
-      setStyle(styles.menu);
+      document.body.classList.remove(styles.bodyNoScroll)
+      setStyle(styles.menu)
     }
-  }, [menuStatus]);
+  }, [menuStatus])
 
   const handleLogoClick = () => {
-    changeCultureFilter(null);
-    changeChemistryFilter(null);
-    changeTypeFilter(null);
-    setMenuStatus(false);
-    navigate('/');
+    changeCultureFilter(null)
+    changeChemistryFilter(null)
+    changeTypeFilter(null)
+    setMenuStatus(false)
+    navigate("/")
   }
 
-  const handleSearch = (e) => {
-    changeSearchResult(e.target.value);
+  const handleSearch = e => {
+    changeSearchResult(e.target.value)
   }
 
   const handleClearSearch = () => {
-    changeSearchResult('');
+    changeSearchResult("")
   }
 
   const handleSetResult = () => {
     if (searchResult.length) {
-      navigate('/products');
+      navigate("/products")
     }
   }
 
@@ -84,21 +86,20 @@ export const Header = () => {
           </div>
         } */}
         <div
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === ' ') {
-              handleLogoClick();
+          onKeyDown={e => {
+            if (e.key === "Enter" || e.key === " ") {
+              handleLogoClick()
             }
           }}
           role="button"
           tabIndex="0"
           className={styles.logoWrapper}
-          onClick={() => handleLogoClick()}>
-          <img alt='logo' src={newLogo} className={styles.logoImg} />
+          onClick={() => handleLogoClick()}
+        >
+          <img alt="logo" src={newLogo} className={styles.logoImg} />
         </div>
         <div className={styles.socialsWrapper}>
-
-          {
-            currentPath !== '/products/' &&
+          {currentPath !== "/products/" && (
             <div className={styles.searchContainer}>
               <div>
                 <input
@@ -106,18 +107,21 @@ export const Header = () => {
                   role="button"
                   className={styles.searchInput}
                   placeholder="Шукати"
-                  onChange={(e) => handleSearch(e)}
+                  onChange={e => handleSearch(e)}
                   value={searchResult}
-                  onKeyDown={(e) => {
+                  onKeyDown={e => {
                     if (e.key === "Enter") {
-                      handleSetResult();
+                      handleSetResult()
                     }
                   }}
                 />
               </div>
-              {searchResult?.length > 0 ?
+              {searchResult?.length > 0 ? (
                 <>
-                  <div className={styles.searchButton} onClick={() => handleSetResult()}>
+                  <div
+                    className={styles.searchButton}
+                    onClick={() => handleSetResult()}
+                  >
                     <p>Шукати</p>
                   </div>
                   <img
@@ -127,59 +131,87 @@ export const Header = () => {
                     onClick={() => handleClearSearch()}
                   />
                 </>
-                :
+              ) : (
                 <img
                   src={searchIcon}
                   className={styles.searchIcon}
                   alt="Search Icon"
                 />
-              }
+              )}
             </div>
-          }
+          )}
 
-          {
-            currentPath !== '/products/' &&
-            <div className={styles.mobileSearchIcon} onClick={() => setMobileSearchModal(true)}>
+          {currentPath !== "/products/" && (
+            <div
+              className={styles.mobileSearchIcon}
+              onClick={() => setMobileSearchModal(true)}
+            >
               <img
                 src={searchIcon}
                 className={styles.searchIcon}
                 alt="Search Icon"
               />
             </div>
-          }
+          )}
 
-          {mobileSeacrhModal && <ModalSearch setMobileSearchModal={setMobileSearchModal} />}
-
-          {/* <div className={styles.social}>
-            <img alt="social" src={tg} />
-          </div>
+          {mobileSeacrhModal && (
+            <ModalSearch setMobileSearchModal={setMobileSearchModal} />
+          )}
 
           <div className={styles.social}>
-            <img alt="social" src={vib} />
-          </div>
-
-          <div className={styles.social}>
-            <img alt="social" src={wa} />
-          </div> */}
-
-          <button className={styles.btnCall}>
-            <a href="tel:+380674711434" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faPhone} size="lg" className={styles.iconPhone} />
+            <a
+              href="https://t.me/+380674711434"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="social" src={tg} />
             </a>
+          </div>
+
+          <div className={styles.social}>
+            <a
+              href="viber://chat?number=%2B380674711434"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="social" src={vib} />
+            </a>
+          </div>
+
+          <div className={styles.social}>
+            <a
+              href="https://wa.me/380674711434"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="social" src={wa} />
+            </a>
+          </div>
+
+          <a
+            href="tel:+380674711434"
+            target="_blank"
+            rel="noreferrer"
+            className={styles.btnCall}
+          >
+            <FontAwesomeIcon
+              icon={faPhone}
+              size="lg"
+              className={styles.iconPhone}
+            />
             Зателефонувати
-          </button>
+          </a>
 
           <div className={styles.backet} onClick={() => handleOpenBacket()}>
             <img alt="backet" src={backet} />
-            {
-              items?.length >= 1 &&
+            {items?.length >= 1 && (
               <div className={styles.backetLength}>
                 <p>{items?.length}</p>
               </div>
-            }
+            )}
           </div>
         </div>
-      </header >
+      </header>
 
       <div>
         <div className={style}>
@@ -205,7 +237,11 @@ export const Header = () => {
 
               <div>
                 <a href="tel:+380674711434" target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faPhone} size="lg" className={styles.iconPhone} />
+                  <FontAwesomeIcon
+                    icon={faPhone}
+                    size="lg"
+                    className={styles.iconPhone}
+                  />
                 </a>
               </div>
             </div>
